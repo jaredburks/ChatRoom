a4ba050ea6e649196d21e4d55fea8107afc88744
jaredburks <jarabu93@gmail.com>
2017-10-02 11:35:17 -0500

    Created queue and dictionary. Created queueMessages method, and keepAcceptingClients method. Still figuring out how to accept clients and receive massages at same time in run method.

--- ChatroomStarter/ChatroomStarter/ChatroomStarter/ChatroomStarter/Server/Server.cs
+++ ChatroomStarter/ChatroomStarter/ChatroomStarter/ChatroomStarter/Server/Server.cs
@@ -15,20 +15,25 @@ namespace Server
     {
         public static Client client;
         TcpListener server;
+        Queue<Message> messages;
+        Dictionary<string, Client> member;
         public Server()
         {
             server = new TcpListener(IPAddress.Parse("127.0.0.1"), 9191);
             server.Start();
+            member = new Dictionary<string, Client>();
+            messages = new Queue<Message>();
         }
-        public void Run()
+        public async void Run()
         {
-            AcceptClient();
-            //Thread acceptUser = new Thread(AcceptClient);
-            //Task<string> message = new Task<string>(client.Recieve);
-            //string text = await message;
-            string message = client.Recieve();
+            //AcceptClient();
+            Thread acceptClient = new Thread(KeepAcceptingClients);
+            acceptClient.Start();
+            
+            Task<string> receiveMessage = new Task<string>(client.Recieve);
+            string message = await receiveMessage;
+            //string message = client.Recieve();
             Respond(message);
-            //Respond(text);
         }
         private void AcceptClient()
         {
@@ -40,9 +45,20 @@ namespace Server
             client = new Client(stream, clientSocket);
             Console.WriteLine(client.UserId + " Connected");
         }
+        public void KeepAcceptingClients()
+        {
+            while (true)
+            {
+                AcceptClient();
+            }
+        }
         private void Respond(string body)
         {
              client.Send(body);
+        }
+        public void QueueMessages()
+        {
+
         }
     }
 }
